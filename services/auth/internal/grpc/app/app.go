3929980e3de3b4f364e315977035148c642901f1
alexey <youth-design@yandex.ru>
2024-04-08 00:22:35 +0300

    Add logger to app

--- services/auth/internal/grpc/app/app.go
+++ services/auth/internal/grpc/app/app.go
@@ -2,16 +2,23 @@ package app
 
 import (
 	"context"
+	"github.com/youth-design/fit_app/libs/logger"
 	fit_app_auth_v0_authv0 "github.com/youth-design/fit_app/protos/gen/go/auth"
 	"google.golang.org/grpc"
 )
 
 type App struct {
 	fit_app_auth_v0_authv0.UnimplementedAuthServer
+	log *logger.Logger
 }
 
-func Register(gRPCServer *grpc.Server) {
-	fit_app_auth_v0_authv0.RegisterAuthServer(gRPCServer, &App{})
+func Register(gRPCServer *grpc.Server, log *logger.Logger) {
+	fit_app_auth_v0_authv0.RegisterAuthServer(
+		gRPCServer,
+		&App{
+			log: log,
+		},
+	)
 }
 
 func (app *App) Register(ctx context.Context, req *fit_app_auth_v0_authv0.RegisterRequest) (*fit_app_auth_v0_authv0.RegisterResponse, error) {
